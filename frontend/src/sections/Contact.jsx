656947fe75f3b5b0fbf024488b49a14872fe889d
dawitYenew.12 <dawityenew.12@gmail.com>
2024-11-02 22:08:42 +0300

    some contets changes

--- frontend/src/sections/Contact.jsx
+++ frontend/src/sections/Contact.jsx
@@ -79,7 +79,7 @@ const Contact = () => {
           <h3 className="head-text">Let's talk</h3>
           <p className="text-lg text-white-600 mt-3">
             Whether you’re looking to build a new website, improve your existing
-            platform, or bring a unique project to life, I’m here to help.
+            platform, I’m here to help.
           </p>
 
           <form
