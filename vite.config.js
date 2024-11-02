12c7387fb9c1acd424bdf151c0acc63371ccae40
dawitYenew.12 <dawityenew.12@gmail.com>
2024-11-02 12:47:39 +0300

    configuration edited

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-// Define __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@react-three/drei': path.resolve(__dirname, 'node_modules/@react-three/drei'),
-    }
+  build: {
+    rollupOptions: {
+      external: ['@react-three/drei'],  // Add the external module here
+    },
   },
   optimizeDeps: {
-    include: ['@react-three/drei']
+    include: ['@react-three/drei'], // Ensure it's included in optimized dependencies
   },
 });
