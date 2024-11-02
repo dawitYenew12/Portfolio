8178601463d530dd93e05191cae15bd9c6a89a0a
dawitYenew.12 <dawityenew.12@gmail.com>
2024-11-02 12:56:00 +0300

    @gsap/react added to rollupOptions

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,10 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['@react-three/drei', 'react-responsive', 'gsap'],  // Add the external modules here
+      external: ['@react-three/drei', 'react-responsive', 'gsap', '@gsap/react'],  // Add the external modules here
     },
   },
   optimizeDeps: {
-    include: ['@react-three/drei', 'react-responsive', 'gsap'], // Ensure both are included in optimized dependencies
+    include: ['@react-three/drei', 'react-responsive', 'gsap', '@gsap/react'], // Ensure both are included in optimized dependencies
   },
 });
