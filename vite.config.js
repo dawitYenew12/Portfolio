6ab1bca0f797725c1f620f780f6e11b81432a8a5
dawitYenew.12 <dawityenew.12@gmail.com>
2024-11-02 13:13:20 +0300

    reversing

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['@react-three/drei', 'react-responsive', 'gsap', '@gsap/react'],  // Add the external modules here
-    },
-  },
-  optimizeDeps: {
-    include: ['@react-three/drei', 'react-responsive', 'gsap', '@gsap/react'], // Ensure both are included in optimized dependencies
-  },
-});
+})
